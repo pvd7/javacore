e8244311ca67ec9fb688c9dd4d2d3b0a141addf6
Pavel <pvdymov@ya.ru>
2018-06-03 22:35:09 +0700

    first

---
+++ src/main/java/ru/geekbrains/lesson2/App.java
@@ -0,0 +1,34 @@
+package ru.geekbrains.lesson2;
+
+import org.apache.commons.lang3.StringUtils;
+import ru.geekbrains.lesson2.exception.MyArrayDataException;
+import ru.geekbrains.lesson2.exception.MyArraySizeException;
+
+public class App {
+    private static final int MAX_LENGTH = 4;
+
+    public static void arrTest(String[][] args) throws MyArraySizeException, MyArrayDataException {
+        if ((args.length > MAX_LENGTH) | (args[0].length > MAX_LENGTH)) throw new MyArraySizeException("миссив превышает максимальную длинну = " + MAX_LENGTH);
+
+        for (int i = 0; i < args.length; i++) {
+            for (int j = 0; j < args[i].length; j++) {
+                if (!(StringUtils.isNumericSpace(args[i][j]))) throw new MyArrayDataException("неверные данные в ячейке [" + i + "][" + j +"]");
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        String[][] arr = {
+                {"2", "1", "", ""}
+                , {"1", "1", "", ""}
+                , {"1", "1", "4 5", "4f4"}
+                , {"3", "1", "", ""}
+//                , {"qwe", "1", "", ""}
+                };
+        try {
+            arrTest(arr);
+        } catch (MyArraySizeException | MyArrayDataException se) {
+            System.out.println(se.getMessage());
+        }
+    }
+}
---
+++ src/main/java/ru/geekbrains/lesson2/exception/MyArrayDataException.java
@@ -0,0 +1,7 @@
+package ru.geekbrains.lesson2.exception;
+
+public class MyArrayDataException extends Throwable {
+    public MyArrayDataException(String message) {
+        super(message);
+    }
+}
