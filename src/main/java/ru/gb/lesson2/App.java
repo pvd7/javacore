50f654aae18553ace5536eaec9ab4d3cbd82c13f
Pavel Dymov <pvdymov@vankoroil.ru>
2018-06-04 10:06:37 +0700

    fix

--- src/main/java/ru/gb/lesson2/App.java
+++ src/main/java/ru/gb/lesson2/App.java
@@ -7,8 +7,8 @@ import ru.gb.lesson2.exception.MyArraySizeException;
 public class App {
     private static final int MAX_LENGTH = 4;
 
-    public static void arrTest(String[][] args) throws MyArraySizeException, MyArrayDataException {
-        if ((args.length > MAX_LENGTH) | (args[0].length > MAX_LENGTH)) throw new MyArraySizeException("миссив превышает максимальную длинну = " + MAX_LENGTH);
+    private static void arrTest(String[][] args) throws MyArraySizeException, MyArrayDataException {
+        if ((args.length > MAX_LENGTH) | (args[0].length > MAX_LENGTH)) throw new MyArraySizeException("массив превышает максимальную длинну = " + MAX_LENGTH);
 
         for (int i = 0; i < args.length; i++) {
             for (int j = 0; j < args[i].length; j++) {
@@ -23,7 +23,7 @@ public class App {
                 , {"1", "1", "", ""}
                 , {"1", "1", "4 5", "4f4"}
                 , {"3", "1", "", ""}
-//                , {"qwe", "1", "", ""}
+                , {"qwe", "1", "", ""}
                 };
         try {
             arrTest(arr);
