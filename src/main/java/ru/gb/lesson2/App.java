6c227db462e8fc94159fd0aacbff25a028e06485
Pavel Dymov <pvdymov@vankoroil.ru>
2018-06-04 10:42:46 +0700

    + String.format()

--- src/main/java/ru/gb/lesson2/App.java
+++ src/main/java/ru/gb/lesson2/App.java
@@ -8,11 +8,10 @@ public class App {
     private static final int MAX_LENGTH = 4;
 
     private static void arrTest(String[][] args) throws MyArraySizeException, MyArrayDataException {
-        if ((args.length > MAX_LENGTH) | (args[0].length > MAX_LENGTH)) throw new MyArraySizeException("массив превышает максимальную длинну = " + MAX_LENGTH);
-
+        if (!((args.length == MAX_LENGTH) & (args[0].length == MAX_LENGTH))) throw new MyArraySizeException(String.format("массив не соответствует размеру [%1$d][%1$d]", MAX_LENGTH));
         for (int i = 0; i < args.length; i++) {
             for (int j = 0; j < args[i].length; j++) {
-                if (!(StringUtils.isNumericSpace(args[i][j]))) throw new MyArrayDataException("неверные данные в ячейке [" + i + "][" + j +"]");
+                if (!(StringUtils.isNumericSpace(args[i][j]))) throw new MyArrayDataException("в ячейке [" + i + "][" + j +"] не int");
             }
         }
     }
@@ -23,12 +22,12 @@ public class App {
                 , {"1", "1", "", ""}
                 , {"1", "1", "4 5", "4f4"}
                 , {"3", "1", "", ""}
-                , {"qwe", "1", "", ""}
+//                , {"qwe", "1", "", ""}
                 };
         try {
             arrTest(arr);
-        } catch (MyArraySizeException | MyArrayDataException se) {
-            System.out.println(se.getMessage());
+        } catch (MyArraySizeException | MyArrayDataException e) {
+            System.out.println(e.getMessage());
         }
     }
 }
