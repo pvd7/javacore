7e306ace9df27005a9d105bdabbbc48a2643bc02
Pavel Dymov <pvdymov@vankoroil.ru>
2018-06-06 17:16:55 +0700

    + comments

--- src/main/java/ru/gb/lesson2/App.java
+++ src/main/java/ru/gb/lesson2/App.java
@@ -9,6 +9,16 @@ import org.apache.commons.lang3.StringUtils;
 import ru.gb.lesson2.exception.MyArrayDataException;
 import ru.gb.lesson2.exception.MyArraySizeException;
 
+/**
+ * 1. Напишите метод, на вход которого подается двумерный строковый массив размером 4х4,
+ * при подаче массива другого размера необходимо бросить исключение MyArraySizeException.
+ * 2. Далее метод должен пройтись по всем элементам массива, преобразовать в int, и просуммировать.
+ * Если в каком-то элементе массива преобразование не удалось (например, в ячейке лежит символ
+ * или текст вместо числа), должно быть брошено исключение MyArrayDataException – с детализацией,
+ * в какой именно ячейке лежат неверные данные.
+ * 3. В методе main() вызвать полученный метод, обработать возможные исключения
+ * MySizeArrayException и MyArrayDataException и вывести результат расчета.
+ */
 public class App {
     private static final int MAX_LENGTH = 4;
 
